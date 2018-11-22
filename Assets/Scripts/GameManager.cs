a014c8f8ca9fc63fa0afd2d716f94c3465f2fd9b
moominpapa <c_p_w_et@naver.com>
2018-11-22 15:39:15 +0900

    받아라 시어머니야

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,26 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
 
     int Ran1 = 0;
     int Ran2 = 0;
     public Text Rantext1;
     public Text Rantext2;
     Text text;
-	// Use this for initialization
-	void Start () {
-
+    public Button[] button;
+    public Text[] t;
+    // Use this for initialization
+    void Start()
+    {
         text = GetComponent<Text>();
         Ran1 = Random.Range(1, 999);
         Ran2 = Random.Range(1, 999);
         Rantext1.text = Ran1.ToString();
         Rantext2.text = Ran2.ToString();
-        text.text = Ran1 + Ran2.ToString();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        text.text = "+";//(Ran1 + Ran2).ToString();
+        for (int i = 0; i < button.Length; ++i)
+        {
+            t[i] = button[i].GetComponentInChildren<Text>();
+        }
+        int[] num = new int[3];
+        num[0] = Random.Range(-1, 1);
+        for (int i = 0; i < t.Length; ++i)
+        {
+            t[i].text = (Ran1 + Ran2 - 1 + i).ToString();
+        }
+    }
 }
