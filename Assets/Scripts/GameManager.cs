5dd0a518d62c3adda1cac0334f908abe1fd3c5c5
GeunK_Academy <kwoonever@gmail.com>
2018-11-22 15:25:28 +0900

    아 너무 쪼시네 진짜

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
+    int Ran1 = 0;
+    int Ran2 = 0;
+    public Text Rantext1;
+    public Text Rantext2;
+    Text text;
 	// Use this for initialization
 	void Start () {
-		
+
+        text = GetComponent<Text>();
+        Ran1 = Random.Range(1, 999);
+        Ran2 = Random.Range(1, 999);
+        Rantext1.text = Ran1.ToString();
+        Rantext2.text = Ran2.ToString();
+        text.text = Ran1 + Ran2.ToString();
 	}
 	
 	// Update is called once per frame
